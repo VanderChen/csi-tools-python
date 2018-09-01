36c816237ab414d315a59cb8cd35d46dbd8faa78
VanderChen <vanderchen@outlook.com>
2018-09-01 19:44:31 +0800

    fix return

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from .read_socket import read_socket
 
 def main():
     read_socket.read_socket_start()
+    while True:
+        print(read_socket.out_data.get())
     pass
 
 if __name__ == '__main__':
